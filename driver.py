276e9ce595f70fa0c28ee978ac0a6294dab9b9ca
Will Bradley <wwbradle@usc.edu>
2024-11-28 15:04:27 -0800

    scannertest

--- driver.py
+++ driver.py
@@ -2,13 +2,15 @@ from mfrc522 import SimpleMFRC522
 
 mrfc = SimpleMFRC522()
 
-while(1):
+
+
+
+while (1):
 	try:
 		id = mrfc.read()
-		print(f"ID: ${id}")
-	except AUTHERROR:
+		print(id)
+		break
+	except:
 		continue
-
-
 
 
